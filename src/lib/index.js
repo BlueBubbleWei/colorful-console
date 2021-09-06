33f4a76cf7a00043042e274422a618072aea672c
韦玉梅 <WEIYUMEI020@pingan.com.cn>
2021-09-06 15:24:36 +0800

    console

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,52 @@
-import luck from './luck.vue'
-const luckyObj = {
-    install(Vue, options) { 
-        Vue.component(luck.name, luck)
+// import luck from './luck.vue'
+// const luckyObj = {
+//     install(Vue, options) { 
+//         Vue.component(luck.name, luck)
+//     }
+// }
+
+// if (typeof window !== 'undefined' && window.vue) { 
+//     window.luckyObj = luckyObj
+//     Vue.use(luckyObj)
+// }
+
+// export default luckyObj
+
+var chalk = require('chalk');
+
+var rainbowColors = {
+  violet: '#9400D3',
+  indigo: '#4B0082',
+  blue: '#0000FF',
+  green: '#00FF00',
+  yellow: '#FFFF00',
+  orange: '#FF7F00',
+  red: '#FF0000',
+  white: '#FFFFFF'
+}
+
+var log = color => {
+  return (...args) => {
+    var len = args.length;
+    var rest = args.splice(1)
+
+    if (typeof window === 'undefined') {
+      // on nodeJS env
+      console.log(chalk.hex(color)(args[0]), ...rest)
+    } else {
+      // on browser env
+      console.log('%c ' + args[0], 'color:' + color, ...rest);
     }
-}
+  };
+};
 
-if (typeof window !== 'undefined' && window.vue) { 
-    window.luckyObj = luckyObj
-    Vue.use(luckyObj)
-}
-
-export default luckyObj
+module.exports = {
+  violet: log(rainbowColors.violet),
+  indigo: log(rainbowColors.indigo),
+  blue: log(rainbowColors.blue),
+  green: log(rainbowColors.green),
+  yellow: log(rainbowColors.yellow),
+  orange: log(rainbowColors.orange),
+  red: log(rainbowColors.red),
+  white: log(rainbowColors.white)
+};
